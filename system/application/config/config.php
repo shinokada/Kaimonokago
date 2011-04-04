b7cb9add8b6e42b2a00c520bddbad52b2ae26c4f
shin <okada.shin@gmail.com>
2011-04-04 09:04:40 -0700

    Edited system/application/config/config.php via GitHub

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -11,7 +11,7 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://localhost/cecilieokada/";
+$config['base_url']	= "http://localhost/kaimonokago/";
 
 /*
 |--------------------------------------------------------------------------
