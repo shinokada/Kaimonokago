3e5ef338b6216d2610fb37fb16646f11f927bf66
shin <okada.shin@gmail.com>
2011-04-04 09:05:41 -0700

    Edited system/application/config/database.php via GitHub

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -39,8 +39,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "emile";
-$db['default']['database'] = "cecilieokada";
+$db['default']['password'] = "password";
+$db['default']['database'] = "kaimonokago";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
